e2b131492aeac0abffa9901a9daa68323756cf32
Molert511 <77402868+Molert511@users.noreply.github.com>
2022-08-15 16:51:16 +0300

    Add files via upload

---
+++ DP1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+#define ll long long
+
+ll const INF = 1e9;
+
+ll solve(vector<ll> &inp, ll is_sum, vector<ll> &means){
+    if (is_sum < 0){
+        return INF;
+    }
+    if (is_sum == 0){
+        means[0] = 0;
+        return 0;
+    }
+    if (means[is_sum] != INF){
+        return means[is_sum];
+    }
+    ll best = INF;
+    for (auto elem: inp){
+        best = min(best, solve(inp, is_sum - elem, means) + 1);
+    }
+    means[is_sum] = best;
+    return best;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n, is_sum;
+    cin >> n >> is_sum;
+    vector<ll> inp(n);
+    for (ll i = 0; i < n; i++){
+        cin >> inp[i];
+    }
+    vector<ll> means(is_sum + 1, INF);
+    cout << solve(inp, is_sum, means);
+    return 0;
+}
